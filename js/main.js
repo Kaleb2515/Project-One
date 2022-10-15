cac8d5b9fddc62c096fa43e1c66a0c44b2c0c49f
Kaleb2515 <zuhars@icloud.com>
2022-10-15 15:33:38 -0500

    tinkered around with javascript got a function to work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
 /*----- constants -----*/
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const randomLetters = letters[Math.floor(Math.random() * letters.length)]
+const possibleLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const possibleArtists = ["Tori Amos", "Bork", "PJ Harvey", "Liz Phair", "Sinead Oconnor", "Fiona Apple", "Alanis Morissette", "Sarah McLachlan", "Dolores ORiordan", "Courtney Love"]
+const randomLetters = possibleLetters[Math.floor(Math.random() * possibleLetters.length)]
+const randomLetter = ""
+// const randomArtist = possibleArtists[Math.floor(Math.random() * possibleArtists.length)]
+
+
 /*----- app's state (variables) -----*/
+let playerChoice = ""
+let correctAnswer = randomArtist
+let maxWrong = 10
+let submittedGuess = null
+
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
+
+
+
 /*----- functions -----*/
+function gameInit(){
+
+}
+
+function randomArtist() {
+    answer = possibleArtists[Math.floor(Math.random()* possibleArtists.length)];
+    alert(answer) 
+}
+
+
+
+
+
+
+// document.getElementById('subBtn').innerHTML = "";
+// document.getElementById('maxWrong').innerHTML = maxWrong;
+
+// document.getElementById('answerField').innerHTML = guess;
+
+randomArtist()
 
 
 console.log(randomLetters)
-
+console.log(randomArtist)
+console.log(correctAnswer)
 
