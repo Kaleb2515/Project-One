a45a73844105ee720fab97bdc526ce2a9a1e30e4
Kaleb2515 <zuhars@icloud.com>
2022-10-15 18:20:40 -0500

    submit button console logs input field now

--- js/main.js
+++ js/main.js
@@ -1,47 +1,65 @@
 /*----- constants -----*/
-const possibleLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const possibleArtists = ["Tori Amos", "Bork", "PJ Harvey", "Liz Phair", "Sinead Oconnor", "Fiona Apple", "Alanis Morissette", "Sarah McLachlan", "Dolores ORiordan", "Courtney Love"]
-const randomLetters = possibleLetters[Math.floor(Math.random() * possibleLetters.length)]
-const randomLetter = ""
-// const randomArtist = possibleArtists[Math.floor(Math.random() * possibleArtists.length)]
+const possibleLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const possibleArtists = [
+  "Tori Amos",
+  "Bork",
+  "PJ Harvey",
+  "Liz Phair",
+  "Sinead Oconnor",
+  "Fiona Apple",
+  "Alanis Morissette",
+  "Sarah McLachlan",
+  "Dolores ORiordan",
+  "Courtney Love",
+];
 
+const guess = document.querySelector("#guess");
+
+const randomLetters =
+  possibleLetters[Math.floor(Math.random() * possibleLetters.length)];
+const randomLetter = "";
+let displayText = guess
 
 /*----- app's state (variables) -----*/
-let playerChoice = ""
-let correctAnswer = randomArtist
-let maxWrong = 10
-let submittedGuess = null
+let playerGuess,
+correctAnswer,
+maxWrong = 10,
+submittedGuess = null;
 
+let state = {
+    playerGuess,
+    correctAnswer,
+    maxWrong,
+    submittedGuess,
+};
 
 /*----- cached element references -----*/
 /*----- event listeners -----*/
-
+// document.getElementById("subBtn").addEventListener("click", displayText);
 
 
 /*----- functions -----*/
-function gameInit(){
-
-}
+function gameInit() {}
 
 function randomArtist() {
-    answer = possibleArtists[Math.floor(Math.random()* possibleArtists.length)];
-    alert(answer) 
+  answer = possibleArtists[Math.floor(Math.random() * possibleArtists.length)];
+  console.log(answer);
 }
-
-
-
-
-
 
 // document.getElementById('subBtn').innerHTML = "";
 // document.getElementById('maxWrong').innerHTML = maxWrong;
 
 // document.getElementById('answerField').innerHTML = guess;
 
-randomArtist()
+// let input = document.getElementById('guess');
 
 
-console.log(randomLetters)
-console.log(randomArtist)
-console.log(correctAnswer)
+function send() {
+    console.log(guess.value)
+}
+
+randomArtist();
+
+console.log(randomLetters);
+console.log(randomArtist);
 
