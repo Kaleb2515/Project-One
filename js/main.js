43635ab03624705d4fc8d6097ed9e7ef1cc52db0
Kaleb2515 <zuhars@icloud.com>
2022-10-15 12:12:44 -0500

    basic layout of page with HTML, CSS and some random guessing in javascript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*----- constants -----*/
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const randomLetters = letters[Math.floor(Math.random() * letters.length)]
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+
+console.log(randomLetters)
+
+
