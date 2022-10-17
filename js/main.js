edcd9d8c3e1c266ffd9e81b88fa01de849d73355
Kaleb2515 <zuhars@icloud.com>
2022-10-17 13:17:42 -0500

    function now recognizes correct and incorrect input indices values and console logs nthem as correct or incorrect

--- js/main.js
+++ js/main.js
@@ -1,76 +1,95 @@
 /*----- constants -----*/
 
 const possibleArtists = [
-  "Tori Amos",
-  "Bjork",
-  "PJ Harvey",
-  "Liz Phair",
-  "Sinead Oconnor",
-  "Fiona Apple",
-  "Alanis Morissette",
-  "Sarah McLachlan",
-  "Dolores ORiordan",
-  "Courtney Love",
-  "Shirley Manson",
-];
+    "Tori Amos",
+    "Bjork",
+    "PJ Harvey",
+    "Liz Phair",
+    "Sinead Oconnor",
+    "Fiona Apple",
+    "Alanis Morissette",
+    "Sarah McLachlan",
+    "Dolores ORiordan",
+    "Courtney Love",
+    "Shirley Manson",
+  ];
+  
+  const guess = document.querySelector("#guess");
+  
+  const answer =
+    possibleArtists[Math.floor(Math.random() * possibleArtists.length)];
+  
+  /*----- app's state (variables) -----*/
+  let playerGuess,
+    correctAnswer,
+    maxGuesses = 10,
+    underScores = [];
+  
+  let state = {
+    playerGuess,
+    correctAnswer,
+    maxGuesses,
+    underScores,
+  };
+  
+  /*----- cached element references -----*/
+  
+  
+  /*----- event listeners -----*/
+  // Pressing down on a letter logs it to console.log
 
-const guess = document.querySelector("#guess");
-
-const answer =
-  possibleArtists[Math.floor(Math.random() * possibleArtists.length)];
-
-/*----- app's state (variables) -----*/
-let playerGuess,
-  correctAnswer,
-  maxGuesses = 10,
-  underScores = [];
-
-let state = {
-  playerGuess,
-  correctAnswer,
-  maxGuesses,
-  underScores,
-};
-
-/*----- cached element references -----*/
-
-
-/*----- event listeners -----*/
-// Pressing down on a letter logs it to console.log
-document.addEventListener('keypress', function (event) {
-    let name = event.key;
-    let code = event.code;
-    console.log(`Key pressed ${name} Key code value: ${code}`);
-}, false);
-
-
-
-
-
-/*----- functions -----*/
-function gameInit() {}
-
-// logs users guess to console log when hit submit button
-function send() {
-  console.log(guess.value);
-}
-
-// Underscores in console log
-function randomNames() {
-  for (let i = 0; i < answer.length; i++) {
-    underScores.push("_");
+  
+  
+  
+  
+  
+  /*----- functions -----*/
+  function gameInit() {}
+  
+  // logs users guess to console log when hit submit button
+  function send() {
+    console.log(guess.value);
+    pGuess()
   }
-  return underScores;
+  
+
+
+  // Underscores in console log
+  function randomNames() {
+    for (let i = 0; i < answer.length; i++) {
+      underScores.push("_");
+    }
+    return underScores;
+  }
+  
+  // Places randomly generated names as underscores in answer field 
+  function answerFieldRandNames() {
+    return answerUnderscores.innerHTML = answer.replace(/[a-z]/gi, '_')
+  
+  }
+
+//  function placeLetter
+
+// ********** */
+function pGuess () {
+    for (let i = 0; i < answer.length; i++) {
+        
+        if (answer[i] === guess.value) {
+            console.log("correct")
+            // placeLetter()
+        } else {
+            console.log("incorrect")
+        }
+        
+    } 
 }
 
-// Places randomly generated names as underscores in answer field 
-function answerFieldRandNames() {
-  return answerUnderscores.innerHTML = answer.replace(/[a-z]/gi, '_')
+    
+//*********** */
 
-}
 
-answerFieldRandNames();
-
-console.log(randomNames());
-console.log(answer);
-
+  
+  answerFieldRandNames();
+  
+  console.log(randomNames());
+  console.log(answer);
